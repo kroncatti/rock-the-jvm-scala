99ef1dfe90d9b44670801ef52081a63c6c427f47
Kaleb Roncatti <kalebroncatti@gmail.com>
2025-04-01 17:51:23 +0100

    feat: abstract class and inheritance

---
+++ scala-beginners/src/lectures/part2OOP/AbstractDataTypes.scala
@@ -0,0 +1,34 @@
+package lectures.part2OOP
+
+object AbstractDataTypes extends App{
+  // abstract class
+  abstract class Animal {
+    val creatureType: String
+    def eat(): Unit
+  }
+
+  class Dog extends Animal {
+    val creatureType: String = "Canine"
+    override def eat(): Unit = println("au au au")
+  }
+  val dog = new Dog
+
+  // traits
+  trait Carnivore {
+    def eat(animal: Animal): Unit
+  }
+
+  trait ColdBlooded
+
+  class Crocodile extends Animal with Carnivore with ColdBlooded {
+    override val creatureType: String = "croc"
+    def eat(): Unit = println("RAW RAW")
+    def eat(animal: Animal): Unit = println(s"I am a ${this.creatureType} I am eating ${animal.creatureType}")
+  }
+  // traits != abstract_classes
+  // abstract_classes can also have real implementations! traits as well!!!
+  // How are they different ?
+  // 1 - Traits DO NOT have constructor params
+  // 2 - You can only extend one class, but you can inherit multiple traits
+  // 3 - traits = behaviour, abstract_class = "thing"
+}
---
+++ scala-beginners/src/lectures/part2OOP/Inheritance.scala
@@ -0,0 +1,58 @@
+package lectures.part2OOP
+
+object Inheritance extends App {
+
+  // single class inheritance, only one class extended at a time
+  class Animal {
+    def eat(): Unit = println("nomnom")
+
+    val creatureType = "wild"
+  }
+
+  class Cat extends Animal {
+    def crunch(): Unit = {
+      eat()
+      println("crunchCrunch")
+    }
+  }
+
+  val cat = new Cat;
+  cat.crunch()
+
+  // Constructors
+  class Person(name: String, age: Int) {
+    def this(name: String) = this(name, 0)
+  }
+
+  class Adult(name: String, age: Int, idCard: String) extends Person(name)
+
+  // Overriding
+  class Dog extends Animal {
+    override val creatureType = "domestic"
+
+    override def eat(): Unit = println("au au au")
+  }
+
+  val dog = new Dog
+  dog.eat()
+  println(dog.creatureType)
+
+  // you can also do the following
+  //  class Dog extends Animal(override val creatureType: String) {
+  //    override val creatureType = "domestic"
+  //    override def eat(): Unit = println("au au au")
+  //  }
+  // val dog = new Dog("flying_animal")
+
+  // type substitution (broad polymorphism)
+  private val unknownAnimal: Animal = new Dog
+  unknownAnimal.eat()
+
+  // overRIDING vs overLOADING
+
+  // preventing overrides
+  // `final` will not allow us to change stuff:
+  // - use `final` on members;
+  // - use `final` on entire classes;
+  // - `sealed` the class;
+}
